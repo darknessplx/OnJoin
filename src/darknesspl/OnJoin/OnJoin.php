e6fa65f44abc9efbed3d46e0a4b27352c5964da1
KanekiLeChomeur <52374013+KanekiLeChomeur@users.noreply.github.com>
2025-01-27 00:03:39 +0100

    Performance fix + Feature
    Performance fix(Less Config Calls)
    New Feature: tip message(Join Message appears above the hotbar)

--- src/darknesspl/OnJoin/OnJoin.php
+++ src/darknesspl/OnJoin/OnJoin.php
@@ -12,27 +12,80 @@ use pocketmine\utils\Config;
 class OnJoin extends PluginBase implements \pocketmine\event\Listener
 {
 
-public function onEnable(): void {
-    $this->getConfig()->save();
+  public array $configdata = [];
+
+  public function onEnable(): void {
+
+    $this->getServer()->getCommandMap()->register("OnJoin", new OnJoinCommand($this));
+    $this->getServer()->getLogger()->info("OnJoin plugin Enabled");
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-}
-public function onDisable(): void {
-  $this->getConfig()->save();
-}
-public function onJoin(PlayerJoinEvent $event): void {
-$player = $event->getPlayer();
-$playerName = $player->getName();
-$joinMessage = $this->getConfig()->get("joinMessage", "§8[§a+§8] §r{PLAYER}");
-$joinMessage = str_replace("{PLAYER}", $playerName, $joinMessage);
+    $this->configdata = $this->getConfig()->getAll();
 
-$event->setJoinMessage($joinMessage);
-}
-public function onLeave(PlayerQuitEvent $event): void {
-$player = $event->getPlayer();
-$playerName = $event->getPlayer()->getName();
-$quitMessage = $this->getConfig()->get("quitMessage", "§8[§c-§8] §r{PLAYER}");
-$quitMessage = str_replace("{PLAYER}", $playerName, $quitMessage);
+  }
 
-$event->setQuitMessage($quitMessage);
+  public function onDisable(): void {
+
+    $this->getServer()->getLogger()->info("§cOnJoin plugin Disabled");
+    $this->getConfig()->setAll($this->configdata);
+    $this->getConfig()->save();
+  
+  }
+  
+  public function onJoin(PlayerJoinEvent $event): void {
+    if(!isset($this->configdata["type"])){
+      $this->configdata["type"] = "message";
+    }
+    if(!isset($this->configdata["joinMessage"])){
+      $this->configdata["joinMessage"] = "§8[§a+§8] §r{PLAYER}";
+    }
+    switch($this->configdata["type"]){
+      default:
+      case "message":
+        $playerName = $event->getPlayer()->getName();
+        $joinMessage = $this->configdata["joinMessage"];
+        $joinMessage = str_replace("{PLAYER}", $playerName, $joinMessage);
+
+        $event->setJoinMessage($joinMessage);
+      break;
+      case "tip":
+        $playerName = $event->getPlayer()->getName();
+        $joinMessage = $this->configdata["joinMessage"];
+        $joinMessage = str_replace("{PLAYER}", $playerName, $joinMessage);
+
+        $event->setJoinMessage("");
+        $this->getServer()->broadcastTip($joinMessage);
+      break;
+    }
+  }
+  public function onLeave(PlayerQuitEvent $event): void {
+    if(!isset($this->configdata["type"])){
+      $this->configdata["type"] = "message";
+    }
+    if(!isset($this->configdata["joinMessage"])){
+      $this->configdata["quitMessage"] = "§8[§c-§8] §r{PLAYER}";
+    }
+    switch($this->configdata["type"]){
+      default:
+      case "message":
+        $playerName = $event->getPlayer()->getName();
+        $quitMessage = $this->configdata["quitMessage"];
+        $quitMessage = str_replace("{PLAYER}", $playerName, $quitMessage);
+
+        $event->setQuitMessage($quitMessage);
+      break;
+      case "tip":
+        $playerName = $event->getPlayer()->getName();
+        $quitMessage = $this->configdata["quitMessage"];
+        $quitMessage = str_replace("{PLAYER}", $playerName, $quitMessage);
+
+        $event->setQuitMessage("");
+        $this->getServer()->broadcastTip($quitMessage);
+      break;
+    }
+    $playerName = $event->getPlayer()->getName();
+    $quitMessage = $this->getConfig()->get("quitMessage", "§8[§c-§8] §r{PLAYER}");
+    $quitMessage = str_replace("{PLAYER}", $playerName, $quitMessage);
+
+    $event->setQuitMessage($quitMessage);
     }
 }
