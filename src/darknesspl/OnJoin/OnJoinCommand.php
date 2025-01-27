31ef2f7d6cf99c671b59cceadfa6d68f19ebc811
ᴰᵃʳᵏⁿᵉˢˢᴾᴸ <ktostamhip@gmail.com>
2025-01-27 10:45:24 +0100

    Update OnJoinCommand.php

--- src/darknesspl/OnJoin/OnJoinCommand.php
+++ src/darknesspl/OnJoin/OnJoinCommand.php
@@ -4,65 +4,115 @@ namespace darknesspl\OnJoin;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use darknesspl\OnJoin\OnJoin;
-use pocketmine\utils\TextFormat as TF;
 use pocketmine\player\Player;
+use pocketmine\utils\TextFormat as TF;
 
-Class OnJoinCommand extends Command{
+class OnJoinCommand extends Command {
 
     private OnJoin $plugin;
 
-    public function __construct(OnJoin $plugin)
-    {
+    public function __construct(OnJoin $plugin) {
+        parent::__construct("onjoin", "Manage OnJoin settings", null, ["oj"]);
         $this->setPermission("onjoin.config");
         $this->plugin = $plugin;
-        parent::__construct("onjoin", "/onjoin help for more information", null, ["oj"]);
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-        if(!$sender instanceof Player) return $sender->sendMessage(TF::RED . "This command is only available in-game");
-        if(!$this->testPermissionSilent($sender)) {
-			$sender->sendMessage(TF::RED . "You do not have permission to use this command");
-			return;
-		}
-        if(!isset($args[0])) return $sender->sendMessage("§cType /onjoin help for the commands list");
-        switch($args[0]){
-            default:
-            case "help":
-                $sender->sendMessage(TF::RED . "/onjoin help > " . TF::YELLOW . "List all available commands.\n" . TF::RED . "/onjoin joinmessage <text> > " . TF::YELLOW . "set a new Join message\n" . TF::RED . "/onjoin quitmessage <text> > " . TF::YELLOW . "set a new Leaving message\n" . TF::RED . "/onjoin settype <message/tip> > " . TF::YELLOW . "Edit the type of Join/Quit Messages");
-            break;
-            case "joinmessage":
-                array_shift($args);
-                $joinmessage = implode(" ", $args);
-                $this->plugin->getConfig()->set("joinMessage", $joinmessage);
-                $this->plugin->getConfig()->save();
-                $this->plugin->configdata["joinMessage"] = $joinmessage;
-                $joinmessage = str_replace("{PLAYER}", $sender->getName(), $joinmessage);
-                $sender->sendMessage(TF::GREEN . "Join Message has been changed\n" . TF::YELLOW . "Preview: " . TF::RESET . $joinmessage);
-            break;
-            case "quitmessage":
-                array_shift($args);
-                $quitmessage = implode(" ", $args);
-                $this->plugin->getConfig()->set("quitMessage", $quitmessage);
-                $this->plugin->getConfig()->save();
-                $this->plugin->configdata["quitMessage"] = $quitmessage;
-                $quitmessage = str_replace("{PLAYER}", $sender->getName(), $quitmessage);
-                $sender->sendMessage(TF::GREEN . "Leaving Message has been changed\n" . TF::YELLOW . "Preview: " . TF::RESET . $quitmessage);
-            break;
-            case "settype":
-                if($args[1] == "message" || $args[1] == "tip"){
-                    $this->plugin->getConfig()->set("type", $args[1]);
-                    $this->plugin->getConfig()->save();
-                    $this->plugin->configdata["type"] = $args[1];
-                    $sender->sendMessage(TF::GREEN . "Join/Quit Message type has been changed to " . TF::YELLOW . $args[1]);
-                }else{
-                    $sender->sendMessage(TF::RED . 'Invalid input, the value should be "message" or "tip".');
-                }
-            break;
-            case "reload":
-                $this->plugin->getConfig()->reload();
-                $this->plugin->configdata = $this->plugin->getConfig()->getAll();
-                $sender->sendMessage(TF::GREEN . "The Config data has been realoaded correctly.");
-            break;
+        if (!$this->testPermissionSilent($sender)) {
+            $sender->sendMessage(TF::RED . "You don't have permission to use this command.");
+            return;
         }
-      }
+
+        if (!isset($args[0])) {
+            $sender->sendMessage("§cType /onjoin help to see the list of commands.");
+            return;
+        }
+
+        switch ($args[0]) {
+            case "help":
+                $this->showHelp($sender);
+                break;
+
+            case "joinmessage":
+                $this->setJoinMessage($sender, $args);
+                break;
+
+            case "quitmessage":
+                $this->setQuitMessage($sender, $args);
+                break;
+
+            case "settype":
+                $this->setMessageType($sender, $args);
+                break;
+
+            case "reload":
+                $this->reloadConfig($sender);
+                break;
+
+            default:
+                $sender->sendMessage(TF::RED . "Unknown subcommand. Type /onjoin help for more information.");
+                break;
+        }
+    }
+
+    private function showHelp(CommandSender $sender): void {
+        $sender->sendMessage(
+            TF::RED . "/onjoin help > " . TF::YELLOW . "List all available commands.\n" .
+            TF::RED . "/onjoin joinmessage <text> > " . TF::YELLOW . "Set a new Join message.\n" .
+            TF::RED . "/onjoin quitmessage <text> > " . TF::YELLOW . "Set a new Quit message.\n" .
+            TF::RED . "/onjoin settype <message/tip> > " . TF::YELLOW . "Change the type of Join/Quit messages.\n" .
+            TF::RED . "/onjoin reload > " . TF::YELLOW . "Reload the plugin configuration."
+        );
+    }
+
+    private function setJoinMessage(CommandSender $sender, array $args): void {
+        array_shift($args);
+        if (empty($args)) {
+            $sender->sendMessage(TF::RED . "Please provide a message.");
+            return;
+        }
+        $joinMessage = implode(" ", $args);
+        $this->plugin->getConfig()->set("joinMessage", $joinMessage);
+        $this->plugin->getConfig()->save();
+        $this->plugin->configdata["joinMessage"] = $joinMessage;
+
+        $preview = str_replace("{PLAYER}", $sender instanceof Player ? $sender->getName() : "Player", $joinMessage);
+        $sender->sendMessage(TF::GREEN . "Join message has been updated to:\n" . TF::YELLOW . $preview);
+    }
+
+    private function setQuitMessage(CommandSender $sender, array $args): void {
+        array_shift($args);
+        if (empty($args)) {
+            $sender->sendMessage(TF::RED . "Please provide a message.");
+            return;
+        }
+        $quitMessage = implode(" ", $args);
+        $this->plugin->getConfig()->set("quitMessage", $quitMessage);
+        $this->plugin->getConfig()->save();
+        $this->plugin->configdata["quitMessage"] = $quitMessage;
+
+        $preview = str_replace("{PLAYER}", $sender instanceof Player ? $sender->getName() : "Player", $quitMessage);
+        $sender->sendMessage(TF::GREEN . "Quit message has been updated to:\n" . TF::YELLOW . $preview);
+    }
+
+    private function setMessageType(CommandSender $sender, array $args): void {
+        if (!isset($args[1])) {
+            $sender->sendMessage(TF::RED . 'Invalid input. Use "/onjoin settype <message/tip>".');
+            return;
+        }
+        if (!in_array($args[1], ["message", "tip"], true)) {
+            $sender->sendMessage(TF::RED . 'Invalid input. The value should be "message" or "tip".');
+            return;
+        }
+        $this->plugin->getConfig()->set("type", $args[1]);
+        $this->plugin->getConfig()->save();
+        $this->plugin->configdata["type"] = $args[1];
+        $sender->sendMessage(TF::GREEN . "Join/Quit message type has been changed to: " . TF::YELLOW . $args[1]);
+    }
+
+    private function reloadConfig(CommandSender $sender): void {
+        $this->plugin->getConfig()->reload();
+        $this->plugin->configdata = $this->plugin->getConfig()->getAll();
+        $sender->sendMessage(TF::GREEN . "Configuration reloaded successfully.");
+    }
+}
