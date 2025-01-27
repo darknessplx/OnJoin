d82b70c9a765bf9b196cc06aa4ee8a5bc8bc1893
ᴰᵃʳᵏⁿᵉˢˢᴾᴸ <ktostamhip@gmail.com>
2025-01-27 08:00:04 +0100

    Update OnJoinCommand.php

--- src/darknesspl/OnJoin/OnJoinCommand.php
+++ src/darknesspl/OnJoin/OnJoinCommand.php
@@ -19,7 +19,7 @@ Class OnJoinCommand extends Command{
         parent::__construct("onjoin", "/onjoin help for more information", null, ["oj"]);
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): string {
         if(!$sender instanceof Player) return $sender->sendMessage(TF::RED . "This command is only available in-game");
         if(!$this->testPermissionSilent($sender)) {
 			$sender->sendMessage(TF::RED . "You do not have permission to use this command");
